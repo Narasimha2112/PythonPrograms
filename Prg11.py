1bb2eb4399dc91181eb28aae3a6cc3dc1ea741af
Narasimha2112 <141565501+Narasimha2112@users.noreply.github.com>
2024-03-03 21:23:13 +0530

    Write a program to check whether the given number is perfect or not

---
+++ Prg11.py
@@ -0,0 +1,11 @@
+#Write a program to check whether the given number is perfect or not
+
+Number = int(input("Enter a Number: "))
+Sum = 0
+for i in range(1, Number):
+    if(Number % i == 0):
+        Sum = Sum + i
+if (Sum == Number):
+    print(" %d is a Perfect Number" %Number)
+else:
+    print(" %d is not a Perfect Number" %Number)
