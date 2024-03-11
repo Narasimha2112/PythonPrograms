5af17735248bb92e995474dbba657f5ca6fa5a70
Narasimha2112 <141565501+Narasimha2112@users.noreply.github.com>
2024-03-11 19:38:36 +0530

    Write a program to split a given string on hyphens and display each substring.

---
+++ Prg18.py
@@ -0,0 +1,11 @@
+"""
+Write a program to split a given string on hyphens and display each substring.
+
+Given:   str1 = "Emma-is-a-data-scientist”
+"""
+
+str1 = "Emma-is-a-data-scientist"
+
+print(str1.split('-'))
+
+    
