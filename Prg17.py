17731a0ae73d7887dc9704ed9d8d02a69e0812d7
Narasimha2112 <141565501+Narasimha2112@users.noreply.github.com>
2024-03-11 19:37:40 +0530

    Write a program to find all occurrences of “USA” in a given string ignoring the case.

---
+++ Prg17.py
@@ -0,0 +1,10 @@
+"""
+Write a program to find all occurrences of “USA” in a given string ignoring the case. 
+
+Given: str1 = "Welcome to USA. usa is awesome, isn't it?”
+"""
+
+x = "Welcome to USA. usa is awesome, isn't it?"
+
+if "USA" in x:
+    print("Yes USA is present")
